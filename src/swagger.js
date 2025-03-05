422f3a02ddc810948d1c1ba7586323d015384e03
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2025-03-04 18:03:31 -0600

    update changes

--- src/swagger.js
+++ src/swagger.js
@@ -1,17 +1,22 @@
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
+// Define la URL del servidor según el entorno
+const host = process.env.NODE_ENV === 'production'
+  ? `https://${process.env.VERCEL_URL}` // En producción, Vercel define VERCEL_URL
+  : 'http://localhost:3000';           // En desarrollo
+
 const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'API Documentación - Neo4j & Node.js ',
+      title: 'API Documentación - Neo4j & Node.js',
       version: '1.0.0',
-      description: 'Documentacion de API que interactúan con Neo4j para la gestión de nodos, relaciones y consultas avanzadas.',
+      description: 'Documentación de API que interactúan con Neo4j para la gestión de nodos, relaciones y consultas avanzadas.',
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: host,
       },
     ],
   },
